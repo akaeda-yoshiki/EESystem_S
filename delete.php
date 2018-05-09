fb64916947cf4a11f0dcf15387e018e84174a275
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-05-09 10:46:47 +0900

    選択されたIDをデータベースから削除を実装

--- delete.php
+++ delete.php
@@ -4,20 +4,12 @@ if(isset($_GET['input_id'])){
     try {
         $db =new PDO('mysql:host=192.168.0.159;dbname=akaeda;','miyashita','sonicdance');
 
+        $id = $_GET['input_id'] - '0';
          // DELETE文を変数に格納
-        $sqldata = $db->prepare('DELETE FROM data WHERE id=2');//+$_GET['input_id']);
-        $sqldata->execute();
-        echo 'DELETE FROM data WHERE id=';
-        echo $_GET['input_id'];
-        // 削除完了のメッセージ
-        //変更された行の数が1かどうか
-        // if($sqldata->affected_rows == 1){
-        //     echo "削除いたしました。";
-        // }else{
-        //     echo "削除失敗です";
-        // }
+        $sqldata = $db->prepare("DELETE FROM data WHERE id=?");
+        $sqldata->execute(array($_GET['input_id']));
         $db=null;
-
+        header( "Location: http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_W/modeselect.html" ) ;
         } catch (Exception $e) {
     }
 }
