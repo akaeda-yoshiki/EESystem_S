b9932da91eb13a75dfd3c1929776b8ec79dd8249
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-05-15 13:30:55 +0900

    入退室データの入力、現在入室している人と過去データを区別して保存

--- insert.php
+++ insert.php
@@ -1,30 +1,84 @@
 <?php
 
  //ここでは送信されたデータをもとにSQLにデータを挿入してきます。
-if(isset($_GET['id']) && isset($_GET['name']) && isset($_GET['grade'])) {
-    // if(isset($_POST['ID']) && isset($_POST['NAME']) && isset($_POST['GRADE'])) {
+if(isset($_GET['id'])) {
+    if(isset($_GET['time'])) {
 
 
-    try{
-        // 接続
-        $db = new PDO('mysql:host=192.168.0.159;dbname=akaeda', 'miyashita', 'sonicdance');
-        echo 'データベース接続成功';
-        $write=$db->prepare('INSERT INTO user (ID, name, grade) VALUES(:id, :name, :grade)');
-        $write->bindvalue(':id',$_GET['id']);
-        $write->bindvalue(':name',$_GET['name']);
-        $write->bindvalue(':grade',$_GET['grade']);
+        try{
+            // 接続
+            $db = new PDO('mysql:host=192.168.0.159;dbname=akaeda', 'miyashita', 'sonicdance');
+            echo 'データベース接続成功<br />';
 
-        $write->execute();
-        // 切断
-        $db = null;
-        header( "Location: http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_W/modeselect.html" ) ;
-    } catch(PDOException $e){
-        echo "データベース接続失敗" . PHP_EOL;
-        echo $e->getMessage();
-        exit;
+            $sqldata = $db->prepare("SELECT id, enter_time FROM now_enter WHERE id = $_GET[id]");
+            $sqldata->execute();
+            // $i = 0;
+            // $db_data[0] = -1;
+            // while ($row = $sqldata->fetch()) {
+            //     $db_data[$i] = $row['id'];
+                
+            //     $i++;
+            //   }
+              while ($row = $sqldata->fetch()) {
+                $db_data[] = array(
+                        'id'=>$row['id'],
+                        'enter_time'=>$row['enter_time']
+                            );
+              }
+        
+            // echo $db_data;
+            
+            if(empty($db_data[0]["id"])){
+                $write=$db->prepare('INSERT INTO now_enter (id, enter_time) VALUES(:id, :enter_time)');
+                $write->bindvalue(':id',$_GET['id']);
+                $write->bindvalue(':enter_time',$_GET['time']);
+                $write->execute();
+                echo "新規追加";
+            }
+            else{
+                $write=$db->prepare('INSERT INTO data (id, enter_time, exit_time) VALUES(:id, :enter_time, :exit_time)');
+                $write->bindvalue(':id',$_GET['id']);
+                $write->bindvalue(':enter_time',$db_data[0]["enter_time"]);
+                $write->bindvalue(':exit_time',$_GET['time']);
+                $write->execute();
+                echo "累積データへ<br />";
+
+                $sqldata = $db->prepare("DELETE FROM now_enter WHERE id = $_GET[id]");
+                $sqldata->execute();
+                echo json_encode($db_data);
+                echo "削除";
+            }
+            // 切断
+            $db = null;
+            // header( "Location: http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_W/modeselect.html" ) ;
+        } catch(PDOException $e){
+            echo "データベース接続失敗" . PHP_EOL;
+            echo $e->getMessage();
+            exit;
+        }
     }
+    else if(isset($_GET['name']) && isset($_GET['grade'])){
+        try{
+            // 接続
+            $db = new PDO('mysql:host=192.168.0.159;dbname=akaeda', 'miyashita', 'sonicdance');
+            echo 'データベース接続成功';
+            $write=$db->prepare('INSERT INTO user (id, name, grade) VALUES(:id, :name, :grade)');
+            $write->bindvalue(':id',$_GET['id']);
+            $write->bindvalue(':name',$_GET['name']);
+            $write->bindvalue(':grade',$_GET['grade']);
     
+            $write->execute();
+            // 切断
+            $db = null;
+            header( "Location: http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_W/modeselect.html" ) ;
+        } catch(PDOException $e){
+            echo "データベース接続失敗" . PHP_EOL;
+            echo $e->getMessage();
+            exit;
+        }
+    }    
 }
+
 
 
 ?>
