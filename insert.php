2a053bbac909bc7df201e293f6a5823f67365e0d
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-05-15 16:59:58 +0900

    データベースに時間の保存を実装

--- insert.php
+++ insert.php
@@ -2,23 +2,29 @@
 
  //ここでは送信されたデータをもとにSQLにデータを挿入してきます。
 if(isset($_GET['id'])) {
-    if(isset($_GET['time'])) {
+    if(isset($_GET['time'])) {//入退室データを入力
 
 
         try{
             // 接続
             $db = new PDO('mysql:host=192.168.0.159;dbname=akaeda', 'miyashita', 'sonicdance');
-            echo 'データベース接続成功<br />';
+            // echo 'データベース接続成功<br />';
+
+            $sqldata = $db->prepare("SELECT *FROM user WHERE id=?");
+            $sqldata->execute(array($_GET['id']));
+         
+            while ($row = $sqldata->fetch()) {
+                 $data[] = array(
+                         'id'=>$row['id'],
+                         'name'=>$row['name'],
+                         'grade'=>$row['grade'],
+                         'stats'=>""
+                             );
+               }
+
 
             $sqldata = $db->prepare("SELECT id, enter_time FROM now_enter WHERE id = $_GET[id]");
             $sqldata->execute();
-            // $i = 0;
-            // $db_data[0] = -1;
-            // while ($row = $sqldata->fetch()) {
-            //     $db_data[$i] = $row['id'];
-                
-            //     $i++;
-            //   }
               while ($row = $sqldata->fetch()) {
                 $db_data[] = array(
                         'id'=>$row['id'],
@@ -33,31 +39,40 @@ if(isset($_GET['id'])) {
                 $write->bindvalue(':id',$_GET['id']);
                 $write->bindvalue(':enter_time',$_GET['time']);
                 $write->execute();
-                echo "新規追加";
+                $data[0]['stats'] = "1";
+                // echo "新規追加";
             }
             else{
-                $write=$db->prepare('INSERT INTO data (id, enter_time, exit_time) VALUES(:id, :enter_time, :exit_time)');
+                $write=$db->prepare('INSERT INTO data (id, enter_time, exit_time, stay) VALUES(:id, :enter_time, :exit_time, :stay)');
                 $write->bindvalue(':id',$_GET['id']);
                 $write->bindvalue(':enter_time',$db_data[0]["enter_time"]);
                 $write->bindvalue(':exit_time',$_GET['time']);
+                $write->bindvalue(':stay',0);
                 $write->execute();
-                echo "累積データへ<br />";
+                // echo "累積データへ<br />";
 
                 $sqldata = $db->prepare("DELETE FROM now_enter WHERE id = $_GET[id]");
                 $sqldata->execute();
-                echo json_encode($db_data);
-                echo "削除";
+                $data[0]['stats'] = "0";
+
+                // echo json_encode($db_data);
+                // echo "削除";
             }
+
+            //JSONデータ出力
+            header("Content-type: application/json; charset=UTF-8");
+            echo json_encode($data);
+                
             // 切断
             $db = null;
             // header( "Location: http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_W/modeselect.html" ) ;
         } catch(PDOException $e){
-            echo "データベース接続失敗" . PHP_EOL;
+            // echo "データベース接続失敗" . PHP_EOL;
             echo $e->getMessage();
             exit;
         }
     }
-    else if(isset($_GET['name']) && isset($_GET['grade'])){
+    else if(isset($_GET['name']) && isset($_GET['grade'])){//新規ユーザ登録
         try{
             // 接続
             $db = new PDO('mysql:host=192.168.0.159;dbname=akaeda', 'miyashita', 'sonicdance');
