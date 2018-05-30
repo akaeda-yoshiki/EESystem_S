1a30cc5809ea2e6cac0821284be373002a8ea49c
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-05-30 18:42:49 +0900

    IDとパスワードの変更

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_GET['id']) && isset($_GET['pass'])) {
-    if($_GET['id'] == "1111" && $_GET['pass'] == "1234")
+    if($_GET['id'] == "rwe" && $_GET['pass'] == "wblrwewblrwe")
         header( "Location: http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_W/modeselect.html" ) ;
     else
         header( "Location: http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_W/login.html" ) ;
